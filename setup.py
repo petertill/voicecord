8afc8953e3343255dede86c3bac2e65df859e06c
Peter Till <tillpetya20@gmail.com>
2024-07-27 15:44:24 +0200

    Initial commit

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
   
 setuptools.setup( 
     name="voicecord", 
-    version="0.0.1", 
+    version="1.0.0", 
     author="Peter Till", 
     author_email="ptertill@gmail.com", 
     packages=["voicecord"], 
