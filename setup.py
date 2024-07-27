426664b555c9c8c3b512fcea64902229efb82f39
Peter Till <tillpetya20@gmail.com>
2024-07-27 14:34:25 +0200

    Package

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
     description="A simple yet powerful voice recording package for Discord", 
     long_description=description, 
     long_description_content_type="text/markdown", 
-    url="https://github.com/gituser/test-tackage", 
+    url="https://github.com/petertill/voicecord", 
     license='MIT', 
     python_requires='>=3.8', 
     install_requires=[] 
