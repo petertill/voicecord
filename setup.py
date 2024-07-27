8cb2be1dbb4baac5015f4bc1986901b063c9910e
Peter Till <tillpetya20@gmail.com>
2024-07-27 20:07:32 +0200

    1.0.5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
   
 setuptools.setup( 
     name="voicecord", 
-    version="1.0.0", 
+    version="1.0.5", 
     author="Peter Till", 
     author_email="ptertill@gmail.com", 
     packages=["voicecord"], 
